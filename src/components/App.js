5609b4cb7f0bfffc13b77be52ed8663d86bdbefb
fithaashraf17 <fithaashraf172@gmail.com>
2023-07-18 21:14:32 +0530

    separated components

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import Stats from "./Stats";
+import PackingList from "./PackingList";
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 12, packed: true },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleReset() {
+    const confirmed = window.confirm("Are you sure you want to delete items");
+    if (confirmed) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItems={handleToggleItem}
+        onResetItem={handleReset}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
